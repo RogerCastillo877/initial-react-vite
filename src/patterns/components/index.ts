34886a34ba7df783356c324e2dc799d872d0dd7f
RogerCastillo877 <roger.castillo877@gmail.com>
2022-12-17 12:02:54 -0500

    To separate components logic

---
+++ src/patterns/components/index.ts
@@ -0,0 +1,6 @@
+export { ProductCard } from './ProductCard';
+export { ProductButtons } from './ProductButtons';
+export { ProductTitle } from './ProductTitle';
+export { ProductImage } from './ProductImage';
+
+
