5e25d2ff5fcee5cb14204b57ec00b1989d67c134
RogerCastillo877 <roger.castillo877@gmail.com>
2022-12-19 22:46:58 -0500

    create shopping cart

--- src/patterns/pages/ShoppingPage.tsx
+++ src/patterns/pages/ShoppingPage.tsx
@@ -26,8 +26,16 @@ export const ShoppingPage = () => {
   const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
 
   const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
-    console.log(count, product);
-
+    setShoppingCart(oldShoppingCart => {
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+        return { ...rest };
+      }
+      return {
+        ...oldShoppingCart,
+        [product.id]: { ...product, count }
+      }
+    })
   };
 
   return (
@@ -59,6 +67,11 @@ export const ShoppingPage = () => {
           <ProductImage className='custom-image' />
           <ProductButtons className='custom-buttons' />
         </ProductCard>
+      </div>
+      <div>
+        <code>
+          {JSON.stringify(shoppingCart, null, 2)}
+        </code>
       </div>
     </div>
   )
