cdda99212472cf4e5ed8bf2a6f66153e25c6a326
RogerCastillo877 <roger.castillo877@gmail.com>
2022-12-20 15:02:34 -0500

    useShoppingCart

--- src/patterns/pages/ShoppingPage.tsx
+++ src/patterns/pages/ShoppingPage.tsx
@@ -2,55 +2,13 @@ import { ProductImage, ProductTitle, ProductButtons, ProductCard } from '../comp
 import '../styles/custom-styles.css';
 import { Product } from '../interfaces/interfaces';
 import { useState } from 'react';
+import { products } from '../data/products';
+import { useShoppingCart } from '../hooks/useShoppingCard';
 
-const product1 = {
-  id: '1',
-  title: 'Coffe Mug - Card',
-  img: './coffee-mug.png'
-}
-
-const product2 = {
-  id: '2',
-  title: 'Coffe Mug - Meme',
-  img: './coffee-mug2.png'
-}
-
-const products: Product[] = [product1, product2];
-
-interface ProductInCart extends Product {
-  count: number
-}
 
 export const ShoppingPage = () => {
 
-  const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
-
-  const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
-    setShoppingCart(oldShoppingCart => {
-
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-
-      if (Math.max(productInCart.count + count, 0)) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart
-        }
-      }
-
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return { ...rest };
-
-      // if (count === 0) {
-      //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      //   return { ...rest };
-      // }
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: { ...product, count }
-      // }
-    })
-  };
+  const { shoppingCart, onProductCountChange } = useShoppingCart();
 
   return (
     <div>
